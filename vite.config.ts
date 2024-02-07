c60cc03daad27899c6f894a290988e6749e18de5
skyeome <tjru7230@naver.com>
2024-02-07 22:43:38 +0900

    fix: vite 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        sw: "./sw.js",
+      },
+    },
+  },
 });
